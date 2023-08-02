5c1b8cfb2caf98b8a8622c1dd05a5c2e83ed2354
Nidhi Sarvaiya <sarvaiya.nidhi@gmail.com>
2023-08-01 22:42:55 -0400

    Initial Commit

---
+++ lib/gem_compat_scan.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "gem_compat_scan/version"
+
+module GemCompatScan
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/gem_compat_scan/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module GemCompatScan
+  VERSION = "0.1.0"
+end
